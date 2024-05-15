baf1bf79b578ff41622eaece2e13b5b513c65e2d
Long Ton <long.ton02@hcmut.edu.vn>
2024-05-15 14:09:29 +0700

    added conversion utility

--- ascon.h
+++ ascon.h
@@ -5,5 +5,16 @@
 #include "ascon128a.h"
 #include "ascon80pq.h"
 #include "ascon-hash.h"
+#include "ascon-hasha.h"
+
+template <class T>
+typename std::enable_if<std::is_integral<T>::value, std::string>::type
+to_little_endian_array(T n) {
+    std::string ret;
+    for (auto i = 0; i < sizeof(n); ++i) {
+        ret += (n >> (i * 8)) & 0xFF;
+    }
+    return ret;
+}
 
 #endif //ASCON_ASCON_H
