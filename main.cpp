17f3d747ec5c6452a5b82c0049d3e8f77dc1802e
Long Ton <long.ton02@hcmut.edu.vn>
2024-05-22 11:51:06 +0700

    added 128a encrypt/decrypt, hash and hash-a to main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@
 #include <map>
 #include "ascon.h"
 
-static const char* short_options = "edk:n:a:i:t:o:h";
+static const char* short_options = "edsEDSk:n:a:i:t:o:h";
 static struct option long_options[] = {
         {"encrypt",         no_argument,        nullptr,'e'},
         {"decrypt",         no_argument,        nullptr,'d'},
+        {"hash",            no_argument,        nullptr,'s'},
+        {"encrypt-a",       no_argument,        nullptr,'E'},
+        {"decrypt-a",       no_argument,        nullptr,'D'},
+        {"hash-a",          no_argument,        nullptr,'S'},
         {"key",             required_argument,  nullptr,'k'},
         {"nonce",           required_argument,  nullptr,'n'},
         {"associated-data", required_argument,  nullptr,'a'},
@@ -23,11 +27,16 @@ static struct option long_options[] = {
 
 void print_help() {
     std::cout << "Usage:    ASCON\r\n"
-                 "          -e|-d|--encrypt|--decrypt\r\n"
+                 "          -e|--encrypt|-d|--decrypt|-E|--encrypt-a|-D|--decrypt-a\r\n"
                  "          -k|--key                <key_path>\r\n"
                  "          -n|--nonce              <nonce_path>\r\n"
                  "          -a|--associated-data    <ad_path>\r\n"
                  "          -t|--tag                <tag_path>\r\n"
+                 "          -i|--input              <input_path>\r\n"
+                 "          -o|--output             <output_path>\r\n"
+                 "\r\n"
+                 "          ASCON\r\n"
+                 "          -s|--hash|-S|--hash-a\r\n"
                  "          -i|--input              <input_path>\r\n"
                  "          -o|--output             <output_path>\r\n";
 }
@@ -44,7 +53,8 @@ to_big_endian_array(T n){
 }
 
 void encrypt(const std::string& k_path, const std::string& n_path, const std::string& a_path,
-             const std::string& p_path, const std::string& output_cipher_path, const std::string& output_tag_path){
+             const std::string& p_path, const std::string& output_cipher_path, const std::string& output_tag_path,
+             bool variant){
     // buffer
     char buffer[4];
 
@@ -105,7 +115,8 @@ void encrypt(const std::string& k_path, const std::string& n_path, const std::st
     nonce_file.close();
 
     // Invoke ascon128_encrypt()
-    auto m = ascon128_encrypt(key, nonce, associated_data, plaintext);
+    auto m = variant ? ascon128a_encrypt(key, nonce, associated_data, plaintext)
+                                        : ascon128_encrypt(key, nonce, associated_data, plaintext);
 
     // get ciphertext and write it to file
     std::remove(output_cipher_path.c_str());
@@ -130,7 +141,8 @@ void encrypt(const std::string& k_path, const std::string& n_path, const std::st
 }
 
 void decrypt(const std::string& k_path, const std::string& n_path, const std::string& a_path,
-             const std::string& c_path, const std::string& t_path, const std::string& output_path){
+             const std::string& c_path, const std::string& t_path, const std::string& output_path,
+             bool variant){
     // buffer
     char buffer[4];
 
@@ -213,7 +225,8 @@ void decrypt(const std::string& k_path, const std::string& n_path, const std::st
     std::remove(output_path.c_str());
     std::ofstream decrypt_file(output_path, std::ios_base::binary | std::ios_base::app);
 
-    auto decrypt_m = ascon128_decrypt(key, nonce, associated_data, encrypt_msg);
+    auto decrypt_m = variant ? ascon128a_decrypt(key, nonce, associated_data, encrypt_msg)
+                                                : ascon128_decrypt(key, nonce, associated_data, encrypt_msg);
     if (decrypt_m){
         std::cout << "Successfully decrypted data" << std::endl;
         decrypt_file.write(decrypt_m.value().c_str(), static_cast<std::streamsize>(decrypt_m.value().length()));
@@ -224,6 +237,32 @@ void decrypt(const std::string& k_path, const std::string& n_path, const std::st
     decrypt_file.close();
 }
 
+void hash(const std::string& i_path, const std::string& o_path, bool variant) {
+    // Get input
+    std::ifstream in_file(i_path, std::ios_base::binary);
+    if (!in_file.is_open()) {
+        std::cerr << "Error opening file: " << i_path << std::endl;
+        return;
+    }
+
+    std::string input((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+
+    in_file.close();
+
+    // Hash
+    auto h = variant ? ascon_hasha(input) : ascon_hash(input);
+
+    // Write to output
+    std::remove(o_path.c_str());
+    std::ofstream out_file(o_path, std::ios_base::binary | std::ios_base::app);
+
+    for (const auto& i : h) {
+        out_file.write((const char *)&i, 1);
+    }
+
+    out_file.close();
+}
+
 static inline bool check_invalid_filepath() {
     if (optarg[0] == '-') {
         std::cerr << "Invalid argument" << std::endl;
@@ -231,9 +270,12 @@ static inline bool check_invalid_filepath() {
     } else return false;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     typedef std::pair<std::string, std::optional<std::string>> map_type;
+
     std::map<std::string, std::optional<std::string>> parsed;
+    bool variant = false;
+    bool enc_mode, dec_mode, hash_mode;
 
     while (true) {
         const auto opt = getopt_long(argc, argv, short_options, long_options, nullptr);
@@ -243,10 +285,19 @@ int main(int argc, char **argv) {
         switch (opt)
         {
             case 'e':
+            case 'E':
                 parsed.insert(map_type("enc", std::nullopt));
+                variant = opt == 'E';
                 break;
             case 'd':
+            case 'D':
                 parsed.insert(map_type("dec", std::nullopt));
+                variant = opt == 'D';
+                break;
+            case 's':
+            case 'S':
+                parsed.insert(map_type("hash", std::nullopt));
+                variant = opt == 'S';
                 break;
             case 'k':
                 if (check_invalid_filepath()) return 1;
@@ -287,27 +338,15 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    std::string key_path;
-    if (parsed.count("key")) {
-        key_path = parsed["key"].value();
-    } else {
-        std::cerr << "No key file specified" << std::endl;
+    enc_mode = parsed.count("enc");
+    dec_mode = parsed.count("dec");
+    hash_mode = parsed.count("hash");
+    if (enc_mode + dec_mode + hash_mode > 1) {
+        std::cerr << "Too many commands" << std::endl;
         return 1;
     }
-
-    std::string nonce_path;
-    if (parsed.count("nonce")) {
-        nonce_path = parsed["nonce"].value();
-    } else {
-        std::cerr << "No nonce file specified" << std::endl;
-        return 1;
-    }
-
-    std::string ad_path;
-    if (parsed.count("ad")) {
-        ad_path = parsed["ad"].value();
-    } else {
-        std::cerr << "No AD file specified" << std::endl;
+    if (enc_mode + dec_mode + hash_mode == 0){
+        std::cerr << "No action specified" << std::endl;
         return 1;
     }
 
@@ -327,21 +366,46 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    std::string tag_path;
-    if (parsed.count("tag")) {
-        tag_path = parsed["tag"].value();
+    if (hash_mode) {
+        hash(input_path, output_path, variant);
     } else {
-        std::cerr << "No tag file specified" << std::endl;
-        return 1;
-    }
+        std::string key_path;
+        if (parsed.count("key")) {
+            key_path = parsed["key"].value();
+        } else {
+            std::cerr << "No key file specified" << std::endl;
+            return 1;
+        }
 
-    if (parsed.count("enc")) {
-        encrypt(key_path, nonce_path, ad_path, input_path, output_path, tag_path);
-    } else if (parsed.count("dec")) {
-        decrypt(key_path, nonce_path, ad_path, input_path, tag_path, output_path);
-    } else {
-        std::cerr << "No action specified" << std::endl;
-        return 1;
+        std::string nonce_path;
+        if (parsed.count("nonce")) {
+            nonce_path = parsed["nonce"].value();
+        } else {
+            std::cerr << "No nonce file specified" << std::endl;
+            return 1;
+        }
+
+        std::string ad_path;
+        if (parsed.count("ad")) {
+            ad_path = parsed["ad"].value();
+        } else {
+            std::cerr << "No AD file specified" << std::endl;
+            return 1;
+        }
+
+        std::string tag_path;
+        if (parsed.count("tag")) {
+            tag_path = parsed["tag"].value();
+        } else {
+            std::cerr << "No tag file specified" << std::endl;
+            return 1;
+        }
+
+        if (enc_mode) {
+            encrypt(key_path, nonce_path, ad_path, input_path, output_path, tag_path, variant);
+        } else if (dec_mode) {
+            decrypt(key_path, nonce_path, ad_path, input_path, tag_path, output_path, variant);
+        }
     }
 
     return 0;
